202c3ea629d490e592c26c1edfe385e2a150b2f3
Isabella Gandl <isabella.c.gandl@gmail.com>
2021-03-02 13:33:22 +0100

    basic build
    Routes, Views, Components

---
+++ src/components/Antragswahl.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="antrag_auswahl" class="hello">
+    <h1>{{ msg }}</h1>
+      <label class="checkbox">
+        <input type="checkbox">Entgeltermäßigung
+      </label>
+      <label class="checkbox">
+        <input type="checkbox">Geschwisterermäßigung
+      </label>
+      <label class="checkbox">
+        <input type="checkbox">Bildungsfond
+      </label>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Antragswahl',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Willkommen</h1>
+    <h2>Erstellen Sie Anträge jetzt vollständig digital</h2>
+    <Antragswahl msg="Welche Anträge möchtest du stellen?"/>
+    <h3>Falls Sie unsicher sind</h3>
+    <p>hilft Ihnen unser Test auf Berechtigung weiter:</p>
+    <button>Welche Anträge sollte ich stellen?</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Antragswahl from '@/components/Antragswahl.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Antragswahl
   }
 }
 </script>
